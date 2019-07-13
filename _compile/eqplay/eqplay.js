ef0f48811456f1d2e5cde25936fd88a4db8bef5f
reedmideke <28969020+reedmideke@users.noreply.github.com>
2019-07-13 11:15:48 -0700

    jump to end after loading to show all quakes

--- _compile/eqplay/eqplay.js
+++ _compile/eqplay/eqplay.js
@@ -209,10 +209,8 @@ var EQPlay={
       }
       this.eqdata=data;
       this.t_total_ms = this.t_end.getTime() - this.t_start.getTime();
-      this.ts_cur = this.t_start.getTime();
       this.infomsg('Date range '+this.fmt_date(this.t_start)+' - ' +this.fmt_date(this.t_end));
-      this.update_data_info();
-      this.update_cur_time_display();
+      this.time_warp_to(this.t_end.getTime());// jump to end, showing all loaded EQ
       return true;
   },
   get_data:function(opts) {
